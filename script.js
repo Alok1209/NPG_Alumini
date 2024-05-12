b93603b9ad17803d75a6946697c44749f1706a7c
divyansh1733 <divyanshk072@gmail.com>
2024-05-12 22:36:04 +0530

    added home page

---
+++ script.js
@@ -0,0 +1,23 @@
+var tl = gsap.timeline()
+gsap.from("#navbar",{
+    y:-100,
+    duration:0.6,
+    delay:0.5,
+    opacity:0
+})
+
+gsap.from("#name h1",{
+    y:100,
+    duration:0.6,
+    delay : 1.1,
+    opacity:0,
+    scale:0
+})
+
+gsap.from("#name h2",{
+    y:100,
+    duration:0.6,
+    delay : 1.1,
+    opacity:0,
+    scale:0
+})
