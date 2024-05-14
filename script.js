2abb50f3328f40daa22e402e7fd3a1c4f02b59a4
divyansh1733 <divyanshk072@gmail.com>
2024-05-14 22:37:05 +0530

    improved landing page

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-// var tl = gsap.timeline()
-// gsap.from("#navbar",{
-//     y:-100,
-//     duration:0.6,
-//     delay:0.5,
-//     opacity:0
-// })
+var tl = gsap.timeline();
+var nav = document.getElementById("#navbar");
+var navContent = document.querySelector("#navbar-content h4");
 
-// gsap.from("#name h1",{
-//     y:100,
-//     duration:0.6,
-//     delay : 1.1,
-//     opacity:0,
-//     scale:0
-// })
+tl.from("#navbar", {
+  y: -100,
+  duration: 0.8,
+  delay: 0.5,
+});
 
-// gsap.from("#name h2",{
-//     y:100,
-//     duration:0.6,
-//     delay : 1.1,
-//     opacity:0,
-//     scale:0
-// })
+tl.from("#navbar-content h4", {
+  y: -100,
+  duration: 0.5,
+  delay: 0.2,
+  stagger: 0.2,
+});
+tl.from("#content h1",{
+    x:-1200,
+    duration:0.5,
+    delay:0.1
+})
+tl.from("#content h2",{
+    x:-1000,
+    duration:0.5,
+    delay:0.1
+})
+gsap.to("#page1 #navbar", {
+  backgroundColor: "white",
+  backdropFilter: blur(0),
+  // document.getElementById("#navbar-content h1").color="white",
+  scrollTrigger: {
+    trigger: "#page1 #navbar",
+    scroller: "body",
+    start: "top -50%",
+    end: "max",
+    // markers:true,
+    scrub: true,
+  },
+});
+
+gsap.to("#page1 #navbar #navbar-content h4", {
+  color: "black",
+  scrollTrigger: {
+    trigger: "#page1 #navbar #navbar-content h4",
+    scroller: "body",
+    start: "top -50%",
+    end: "max",
+    scrub: true,
+  },
+});
+
+navContent.addEventListener("mouseover", function () {
+  gsap.from("#navbar-content span", {
+    y:-50,
+    duration: 0.5,
+  });
+  gsap.to("#navbar-content span",{
+    y:0,
+    duration:0.5
+  })
+});
+
+
+
